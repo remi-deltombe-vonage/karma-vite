3d089b9cd0aa29fd03ea751fcaae9eba420c56ab
Remi Deltombe <remi.deltombe@vonage.com>
2022-10-14 10:48:50 +0200

    fix: fix client loading

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
 export const VITE_CLIENT_ENTRY = require.resolve(
-  'karma-vite/dist/viteClientMock.js',
+  '@vonage/karma-vite/dist/viteClientMock.js',
 );
 export const VITE_FS_PREFIX = '/@fs/';
 export const REWRITE_URL_KEY = '__karma_vite_rewrite_url__';
